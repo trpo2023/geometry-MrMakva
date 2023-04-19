b6e014c26aaeefb9da75c0c910da76ae6e9ba53b
Vadim Makienko <MrMakVa@users.noreply.github.com>
2023-04-19 19:03:30 +0700

    Application assembly

--- obj/src/geometry/geometry.d
+++ obj/src/geometry/geometry.d
@@ -1,3 +1,4 @@
 obj/src/geometry/geometry.o: src/geometry/geometry.c \
  src/libgeometry/libgeometry.h
+
 src/libgeometry/libgeometry.h:
---
+++ obj/src/libgeometry/libgeometry.d
@@ -0,0 +1,4 @@
+obj/src/libgeometry/libgeometry.o: src/libgeometry/libgeometry.c \
+ src/libgeometry/libgeometry.h
+
+src/libgeometry/libgeometry.h:
