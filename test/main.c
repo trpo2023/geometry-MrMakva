e841e4f1b196c873eaef928857fd1cbfe7d9aed4
Vadim Makienko <MrMakVa@users.noreply.github.com>
2023-04-23 14:05:19 +0700

    Add main.c

---
+++ test/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define CTEST_MAIN
+
+#define CTEST_SEGFAULT
+
+#include "C:/Users/latsu/GitHub_projects/geometry/geometry-MrMakva/thirdparty/libgeometry.h"
+
+int main(int argc, const char* argv[])
+{
+    int result = ctest_main(argc, argv);
+
+    return result;
+}
