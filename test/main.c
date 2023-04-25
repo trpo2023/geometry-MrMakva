6e6f03dab9488f95c07a7a8bd933cec63d4701c2
Vadim Makienko <MrMakVa@users.noreply.github.com>
2023-04-25 12:44:50 +0700

    Delete trash

--- test/main.c
+++ test/main.c
@@ -1,17 +1,8 @@
-#include <stdio.h>
-//#include <ctest.h>
-#include "ctest.h"
-//#include "thirdparty/ctest.h"
-
 #define CTEST_MAIN
 
-#define CTEST_SEGFAULT
+#include <ctest.h>
 
-#include "libgeometry/libgeometry.h"
-
-int main(int argc, const char* argv[])
+int main(int argc, const char** argv)
 {
-    int result = ctest_main(argc, argv);
-
-    return result;
+    return ctest_main(argc, argv);
 }
